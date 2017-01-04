d1088af8a4cb6baae3ce0beee13935acb0c75b3a
Sam Messina <samuel.messina@gmail.com>
2017-01-03 19:38:52 -0600

    Added routes for start page and prelim "First" page

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {ReactRouter, Router, Route, hashHistory, IndexRoute} from 'react-router';
 import  Main from '../components/Main';
 import Home from '../components/Home';
+import Start from '../components/Start';
+import First from '../components/First';
 
 let routes = (
     <Router history={hashHistory}>
         <Route path='/' component={Main}>
             <IndexRoute component={Home} />
+            <Route path='/first' component={First}/>
+            <Route path='/start' component={Start}/>
         </Route>
     </Router>
 )
