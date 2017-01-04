b6f3730d7c4ee40048434905c06b3e3ffbce5162
Sam Messina <samuel.messina@gmail.com>
2017-01-03 19:38:13 -0600

    Finished homepage

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,8 +6,17 @@ class Home extends React.Component {
         return (
             <div>
                 <h1>Conversational Git</h1>
-                <p className="lead">Learn git without the techno babble</p>
-                <Link to="/start">
+                <p className="lead"><i>Learn git without the techno babble</i></p>
+                <br/>
+                <p>
+            This guide is a step-by-step of just one type of workflow in git. It is by no means an exhaustive illustration, but it should at least get you started on the right track.
+                </p>
+                    <br/>
+                <p>
+                    I firmly believe the best way to learn anything, especially git, is through practice. With this guide, you can hopefully learn enough to get to a point where you can practice with confidence.
+                </p>
+                    <br/>
+                <Link to="/first">
                     <button type="button" className="btn btn-lg btn-black">Get Started</button>
                 </Link>
             </div>
