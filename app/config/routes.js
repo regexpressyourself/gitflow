6a73ba98752620c5c6a68f1142a6bc49ee6eb9e3
Sam Messina <samuel.messina@gmail.com>
2017-01-07 09:54:14 -0600

    Changed routes to only show the flow component
    flow will control the chart from there

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,16 +9,14 @@ import Home         from '../components/Home';
 import Start        from '../components/Start';
 import First        from '../components/First';
 import Flow         from '../components/Flow';
-import Init         from '../components/Init';
-
+/* import Init         from '../components/Init';
+ * */
 let routes = (
     <Router history={hashHistory}>
         <Route path='/' component={Main}>
             <IndexRoute component={Home} />
             <Route path='/first' component={First}/>
-            <Route component={Flow}>
-                <Route path='/flow/start' component={Start}/>
-                <Route path='/flow/init' component={Init}/>
+            <Route path='/flow' component={Flow}>
             </Route>
         </Route>
     </Router>
